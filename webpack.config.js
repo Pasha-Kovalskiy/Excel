c02f53276552c92f7e49f4cb19872da2c654bfe9
Pasha Kovalskiy <kovalskyi08@gmail.com>
2022-03-29 22:51:14 +0300

    Finish webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,51 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const isProd = process.env.NODE_ENV === "production";
 const isDev = !isProd;
 
-const filename = extension => isDev ? `bundle.${extension}` : `bundle.[hash].${extension}`
+const filename = (extension) =>
+    isDev ? `bundle.${extension}` : `bundle.[hash].${extension}`;
+
+const jsLoaders = () => {
+    const loaders = [
+        {
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env"],
+            },
+        },
+    ];
+
+    if (isDev) {
+        loaders.push({
+            loader: "eslint-loader",
+            options: {},
+        });
+    }
+
+    return loaders;
+};
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
     mode: "development",
-    entry: [
-        "@babel/polyfill",
-        "./index.js"
-    ],
+    entry: ["@babel/polyfill", "./index.js"],
     output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, "dist")
+        filename: filename("js"),
+        path: path.resolve(__dirname, "dist"),
     },
     resolve: {
         extensions: [".js"],
         alias: {
-            '@': path.resolve(__dirname, "src"),
-            '@core': path.resolve(__dirname, "src/core")
-        }
+            "@": path.resolve(__dirname, "src"),
+            "@core": path.resolve(__dirname, "src/core"),
+        },
     },
-    devtool: isDev ? 'source-map' : false,
+    devtool: isDev ? "source-map" : false,
     devServer: {
         port: 3000,
         hot: isDev,
@@ -37,42 +55,33 @@ module.exports = {
             template: "index.html",
             minify: {
                 removeComments: isProd,
-                collapseWhitespace: isProd
-            }
+                collapseWhitespace: isProd,
+            },
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
             patterns: [
                 {
                     from: path.resolve(__dirname, "src/favicon.ico"),
-                    to: path.resolve(__dirname, "dist")
-                }
-            ]
+                    to: path.resolve(__dirname, "dist"),
+                },
+            ],
         }),
         new MiniCssExtractPlugin({
-            filename: filename('css')
+            filename: filename("css"),
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                ],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
+                use: jsLoaders(),
+            },
         ],
     },
 };
